20dc9f3f7e25194249824c9fb57dc5b5b1e7cffb
Abhijeet Singh <106686793+abhijeetsinghCXT@users.noreply.github.com>
2022-07-04 10:17:08 +0530

    New Programs added

---
+++ Mult. Table using Do While/MTDWL.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int num, i=2, m;
+cout<<"Enter the Number below -"<<endl;
+cin>>num;
+
+do {
+    m=num*i;
+   cout<<m<<endl;
+    i++;
+} while (i<11);
+
+
+
+}
